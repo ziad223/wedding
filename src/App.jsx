de44e0b5b08421529fbbd92b0a2548017df3fec6
ziad abdalla <ziadabdalla846@gmail.com>
2024-11-02 23:26:13 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 function App() {
   return (
+    <div className="app">
     <BrowserRouter>
     <Navbar/>
       <Routes>
@@ -22,6 +23,7 @@ function App() {
       </Routes>
       <Footer/>
     </BrowserRouter>
+    </div>
   );
 }
 
