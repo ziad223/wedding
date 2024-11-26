98d8b8a69366805c8781c595b218067bc594863f
ziad abdalla <ziadabdalla846@gmail.com>
2024-11-26 02:38:48 +0200

    last update

--- src/components/AboutSection1.jsx
+++ src/components/AboutSection1.jsx
@@ -6,7 +6,7 @@ const AboutSection1 = () => {
         <div className="bg-white p-10 px-4 md:px-16 lg:px-24 flex flex-col gap-5 lg:flex-row items-center lg:items-start space-y-6 lg:space-y-0 lg:space-x-6">
             {/* القسم الأول */}
             <div className="w-full lg:w-1/3 text-center lg:text-left lg:ml-5">
-                <h2 className="text-2xl font-bold text-[#907457]">رؤيتنا ورسالتنا</h2>
+                <h2 className="text-2xl lg:text-[48px] font-bold text-[#907457]">رؤيتنا ورسالتنا</h2>
             </div>
 
             {/* صورة في الوسط */}
@@ -19,7 +19,7 @@ const AboutSection1 = () => {
             </div>
 
             {/* النص محاذٍ لأسفل الصورة */}
-            <div className="w-full lg:w-1/3 lg:text-lg text-center lg:text-right lg:self-end">
+            <div className="w-full lg:w-1/3  lg:text-[28px] font-light text-center lg:text-right lg:self-end">
                 <p>
                     نسعى إلى تقديم فساتين زفاف تحكي قصة كل عروس، تجمع بين الحرفية العالية والتصاميم العصرية التي تجعل كل لحظة في حفل الزفاف لا تُنسى
                 </p>
